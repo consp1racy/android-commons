9769bcfe91bfec853868cb4ae7491af6772fa47a
Eugen Pechanec <e.pechanec@gmail.com>
2016-09-06 14:40:21 +0200

    Learned multiple bounds generics ^^

--- android/src/main/java/net/xpece/android/app/ViewContainer.kt
+++ android/src/main/java/net/xpece/android/app/ViewContainer.kt
@@ -1,18 +1,18 @@
 package net.xpece.android.app
 
-import android.R
+import android.app.Activity
 import android.view.ViewGroup
 
 /** An indirection which allows controlling the root container used for each activity.  */
 interface ViewContainer {
     /** The root [ViewGroup] into which the activity should place its contents.  */
-    fun <T : BaseActivity> forActivity(activity: T): ViewGroup
+    fun <T> forActivity(activity: T): ViewGroup where T : Activity, T : CustomRootActivity
 
     companion object {
         /** An [ViewContainer] which returns the normal activity content view.  */
         val DEFAULT = object : ViewContainer {
-            override fun <T : BaseActivity> forActivity(activity: T): ViewGroup {
-                return activity.findViewById(R.id.content) as ViewGroup
+            override fun <T> forActivity(activity: T): ViewGroup where T : Activity, T : CustomRootActivity {
+                return activity.findViewById(android.R.id.content) as ViewGroup
             }
         }
     }
