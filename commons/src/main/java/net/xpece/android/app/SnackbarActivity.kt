80f50ded15f7fc58c5d79a6dda9819a2266fd127
consp1racy <e.pechanec@gmail.com>
2016-12-14 18:53:13 +0100

    Version 0.15.0 Support libs 25.1.0 SnackbarActivity contains incompatible changes with support libs < 25.1.0

--- commons/src/main/java/net/xpece/android/app/SnackbarActivity.kt
+++ commons/src/main/java/net/xpece/android/app/SnackbarActivity.kt
@@ -3,6 +3,7 @@ package net.xpece.android.app
 import android.support.annotation.ColorRes
 import android.support.annotation.StringRes
 import android.support.design.widget.Snackbar
+import android.support.design.widget.BaseTransientBottomBar
 import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.content.res.AppCompatResources
 import android.view.View
@@ -15,12 +16,12 @@ interface SnackbarActivity {
 
     var snackbar : Snackbar?
 
-    fun showSnackbar(@StringRes textId: Int, @Snackbar.Duration duration: Int, @ColorRes backgroundId: Int? = null)  {
+    fun showSnackbar(@StringRes textId: Int, @BaseTransientBottomBar.Duration duration: Int, @ColorRes backgroundId: Int? = null)  {
         val context = snackbarParent.context
         showSnackbar(context.getString(textId), duration, backgroundId)
     }
 
-    fun showSnackbar(text: CharSequence, @Snackbar.Duration duration: Int, @ColorRes backgroundId: Int? = null) {
+    fun showSnackbar(text: CharSequence, @BaseTransientBottomBar.Duration duration: Int, @ColorRes backgroundId: Int? = null) {
         val snackbar = Snackbar.make(snackbarParent, text, duration)
         if (backgroundId != null) {
             val view = snackbar.view
