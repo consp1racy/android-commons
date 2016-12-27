ab4fe820971deb7a42c5608795cfb96a6e0d2f52
Eugen Pechanec <e.pechanec@gmail.com>
2016-12-27 23:43:11 +0100

    XpAppCompatBackgroundHelper doesnt log errors

--- commons/src/main/java/android/support/v7/widget/XpAppCompatBackgroundHelper.java
+++ commons/src/main/java/android/support/v7/widget/XpAppCompatBackgroundHelper.java
@@ -3,7 +3,6 @@ package android.support.v7.widget;
 import android.view.View;
 
 import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by Eugen on 26.12.2016.
@@ -19,17 +18,17 @@ class XpAppCompatBackgroundHelper {
         try {
             ctor2420 = cls.getConstructor(View.class, AppCompatDrawableManager.class);
         } catch (NoSuchMethodException e) {
-            e.printStackTrace();
+            //
         }
         CTOR_2420 = ctor2420;
     }
 
     public static AppCompatBackgroundHelper create(View view) {
         try {
-            // Support libraries >= 24.0.0.
+            // Support libraries >= 24.2.0.
             return new AppCompatBackgroundHelper(view);
         } catch (NoSuchMethodError ex) {
-            // Support libraries >= 24.2.0.
+            // Support libraries < 24.2.0.
             return createOnSupportLibs2420(view);
         }
     }
@@ -37,13 +36,8 @@ class XpAppCompatBackgroundHelper {
     private static AppCompatBackgroundHelper createOnSupportLibs2420(final View view) {
         try {
             return CTOR_2420.newInstance(view, AppCompatDrawableManager.get());
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        return null;
     }
 }
