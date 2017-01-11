6212bcb05e8784f7e337fc18319fbe02c34bb33d
consp1racy <e.pechanec@gmail.com>
2017-01-12 00:59:14 +0100

    XpFragment.showAllowingStateLoss

--- commons/src/main/java/net/xpece/android/app/XpFragment.kt
+++ commons/src/main/java/net/xpece/android/app/XpFragment.kt
@@ -2,14 +2,17 @@
 
 package net.xpece.android.app
 
+import android.support.v4.app.DialogFragment
 import android.support.v4.app.Fragment
-import android.support.v4.app.updateLoaderManagerHostController
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
 
 /**
  * Created by Eugen on 29.10.2016.
  */
 
 fun Fragment.invalidateOptionsMenu() = activity?.apply { supportInvalidateOptionsMenu() }
+
 fun android.app.Fragment.invalidateOptionsMenu() = activity?.apply { invalidateOptionsMenu() }
 
 /**
@@ -22,4 +25,34 @@ fun android.app.Fragment.invalidateOptionsMenu() = activity?.apply { invalidateO
  */
 fun Fragment.updateLoaderManagerHostController() {
     updateLoaderManagerHostController()
+}
+
+fun DialogFragment.showAllowingStateLoss(fragmentManager: FragmentManager, tag: String) {
+    try {
+        show(fragmentManager, tag)
+    } catch (ex :IllegalStateException) {
+        //
+    }
+}
+fun DialogFragment.showAllowingStateLoss(fragmentTransaction: FragmentTransaction, tag: String) {
+    try {
+        show(fragmentTransaction, tag)
+    } catch (ex :IllegalStateException) {
+        //
+    }
+}
+
+fun android.app.DialogFragment.showAllowingStateLoss(fragmentManager: android.app.FragmentManager, tag: String) {
+    try {
+        show(fragmentManager, tag)
+    } catch (ex :IllegalStateException) {
+        //
+    }
+}
+fun android.app.DialogFragment.showAllowingStateLoss(fragmentTransaction: android.app.FragmentTransaction, tag: String) {
+    try {
+        show(fragmentTransaction, tag)
+    } catch (ex :IllegalStateException) {
+        //
+    }
 }
