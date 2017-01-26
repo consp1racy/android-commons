3f23500d715c0b26f511e37046336802754a8ad4
consp1racy <e.pechanec@gmail.com>
2017-01-26 22:47:27 +0100

    Commons v0.20.3 Added more JSR-310 moshi converters

--- commons/src/main/java/net/xpece/android/json/ThreeTenBpAdapter.kt
+++ commons/src/main/java/net/xpece/android/json/ThreeTenBpAdapter.kt
@@ -2,15 +2,20 @@ package net.xpece.android.json
 
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
-import org.threeten.bp.LocalDate
-import org.threeten.bp.LocalDateTime
-import org.threeten.bp.LocalTime
-import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.*
 
 /**
  * @author Eugen on 27.08.2016.
  */
 object ThreeTenBpAdapter {
+    @ToJson fun toJson(dt: Instant): String {
+        return dt.toString()
+    }
+
+    @ToJson fun toJson(dt: ZonedDateTime): String {
+        return dt.toString()
+    }
+
     @ToJson fun toJson(dt: OffsetDateTime): String {
         return dt.toString()
     }
@@ -27,6 +32,14 @@ object ThreeTenBpAdapter {
         return dt.toString()
     }
 
+    @ToJson fun toJson(dt: OffsetTime): String {
+        return dt.toString()
+    }
+
+    @FromJson fun offsetTimeFromJson(dt: String): OffsetTime {
+        return OffsetTime.parse(dt)
+    }
+
     @FromJson fun localTimeFromJson(dt: String): LocalTime {
         return LocalTime.parse(dt)
     }
@@ -41,5 +54,13 @@ object ThreeTenBpAdapter {
 
     @FromJson fun offsetDateTimeFromJson(dt: String): OffsetDateTime {
         return OffsetDateTime.parse(dt)
+    }
+
+    @FromJson fun zonedDateTimeFromJson(dt: String): ZonedDateTime {
+        return ZonedDateTime.parse(dt)
+    }
+
+    @FromJson fun instantFromJson(dt: String): Instant {
+        return OffsetDateTime.parse(dt).toInstant() // Preserve zone internally.
     }
 }
