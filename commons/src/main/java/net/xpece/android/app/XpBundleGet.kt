bc6de59b326db7d55a65e5b0015914895744da55
consp1racy <e.pechanec@gmail.com>
2017-02-03 17:20:05 +0100

    Commons v0.21.0 Kotlin 1.1.0-beta-38 XpBundle reading/writing properties impl

---
+++ commons/src/main/java/net/xpece/android/app/XpBundleGet.kt
@@ -0,0 +1,185 @@
+@file:JvmMultifileClass
+@file:JvmName("XpBundle")
+
+package net.xpece.android.app
+
+import android.annotation.TargetApi
+import android.os.Bundle
+import android.os.IBinder
+import android.os.Parcelable
+import android.support.annotation.RequiresApi
+import android.util.Size
+import android.util.SizeF
+import android.util.SparseArray
+import java.io.Serializable
+import kotlin.reflect.KMutableProperty0
+
+/// BaseBundle
+
+@JvmName("getBoolean")
+fun KMutableProperty0<Boolean>.get(bundle: Bundle) {
+    set(bundle.getBoolean(name))
+}
+
+@JvmName("getByte")
+fun KMutableProperty0<Byte>.get(bundle: Bundle) {
+    set(bundle.getByte(name))
+}
+
+@JvmName("getChar")
+fun KMutableProperty0<Char>.get(bundle: Bundle) {
+    set(bundle.getChar(name))
+}
+
+@JvmName("getShort")
+fun KMutableProperty0<Short>.get(bundle: Bundle) {
+    set(bundle.getShort(name))
+}
+
+@JvmName("getInt")
+fun KMutableProperty0<Int>.get(bundle: Bundle) {
+    set(bundle.getInt(name))
+}
+
+@JvmName("getLong")
+fun KMutableProperty0<Long>.get(bundle: Bundle) {
+    set(bundle.getLong(name))
+}
+
+@JvmName("getFloat")
+fun KMutableProperty0<Float>.get(bundle: Bundle) {
+    set(bundle.getFloat(name))
+}
+
+@JvmName("getDouble")
+fun KMutableProperty0<Double>.get(bundle: Bundle) {
+    set(bundle.getDouble(name))
+}
+
+@JvmName("getString")
+fun KMutableProperty0<String>.get(bundle: Bundle) {
+    set(bundle.getString(name))
+}
+
+@JvmName("getCharSequence")
+fun KMutableProperty0<CharSequence>.get(bundle: Bundle) {
+    set(bundle.getCharSequence(name))
+}
+
+@JvmName("getIntegerArrayList")
+fun KMutableProperty0<ArrayList<Int>>.get(bundle: Bundle) {
+    set(bundle.getIntegerArrayList(name))
+}
+
+@JvmName("getStringArrayList")
+fun KMutableProperty0<ArrayList<String>>.get(bundle: Bundle) {
+    set(bundle.getStringArrayList(name))
+}
+
+@JvmName("getCharSequenceArrayList")
+fun KMutableProperty0<ArrayList<CharSequence>>.get(bundle: Bundle) {
+    set(bundle.getCharSequenceArrayList(name))
+}
+
+@JvmName("getSerializable")
+fun KMutableProperty0<Serializable>.get(bundle: Bundle) {
+    set(bundle.getSerializable(name))
+}
+
+@JvmName("getBooleanArray")
+fun KMutableProperty0<BooleanArray>.get(bundle: Bundle) {
+    set(bundle.getBooleanArray(name))
+}
+
+@JvmName("getByteArray")
+fun KMutableProperty0<ByteArray>.get(bundle: Bundle) {
+    set(bundle.getByteArray(name))
+}
+
+@JvmName("getCharArray")
+fun KMutableProperty0<CharArray>.get(bundle: Bundle) {
+    set(bundle.getCharArray(name))
+}
+
+@JvmName("getShortArray")
+fun KMutableProperty0<ShortArray>.get(bundle: Bundle) {
+    set(bundle.getShortArray(name))
+}
+
+@JvmName("getIntArray")
+fun KMutableProperty0<IntArray>.get(bundle: Bundle) {
+    set(bundle.getIntArray(name))
+}
+
+@JvmName("getLongArray")
+fun KMutableProperty0<LongArray>.get(bundle: Bundle) {
+    set(bundle.getLongArray(name))
+}
+
+@JvmName("getFloatArray")
+fun KMutableProperty0<FloatArray>.get(bundle: Bundle) {
+    set(bundle.getFloatArray(name))
+}
+
+@JvmName("getDoubleArray")
+fun KMutableProperty0<DoubleArray>.get(bundle: Bundle) {
+    set(bundle.getDoubleArray(name))
+}
+
+@JvmName("getStringArray")
+fun KMutableProperty0<Array<String>>.get(bundle: Bundle) {
+    set(bundle.getStringArray(name))
+}
+
+@JvmName("getCharSequenceArray")
+fun KMutableProperty0<Array<CharSequence>>.get(bundle: Bundle) {
+    set(bundle.getCharSequenceArray(name))
+}
+
+/// Bundle
+
+@JvmName("getParcelable")
+fun <T : Parcelable> KMutableProperty0<T>.get(bundle: Bundle) {
+    set(bundle.getParcelable(name))
+}
+
+@RequiresApi(21)
+@TargetApi(21)
+@JvmName("getSize")
+fun KMutableProperty0<Size>.get(bundle: Bundle) {
+    set(bundle.getSize(name))
+}
+
+@RequiresApi(21)
+@TargetApi(21)
+@JvmName("getSizeF")
+fun KMutableProperty0<SizeF>.get(bundle: Bundle) {
+    set(bundle.getSizeF(name))
+}
+
+@JvmName("getParcelableArray")
+fun KMutableProperty0<Array<out Parcelable>>.get(bundle : Bundle) {
+    set(bundle.getParcelableArray(name))
+}
+
+@JvmName("getParcelableArrayList")
+fun <T : Parcelable> KMutableProperty0<ArrayList<out T>>.get(bundle: Bundle) {
+    set(bundle.getParcelableArrayList(name))
+}
+
+@JvmName("getSparseParcelableArray")
+fun <T : Parcelable> KMutableProperty0<SparseArray<out T>>.get(bundle: Bundle) {
+    set(bundle.getSparseParcelableArray(name))
+}
+
+@JvmName("getBundle")
+fun KMutableProperty0<Bundle>.get(bundle: Bundle) {
+    set(bundle.getBundle(name))
+}
+
+@RequiresApi(18)
+@TargetApi(18)
+@JvmName("getBinder")
+fun KMutableProperty0<IBinder>.get(bundle: Bundle) {
+    set(bundle.getBinder(name))
+}
---
+++ commons/src/main/java/net/xpece/android/app/XpBundlePut.kt
@@ -0,0 +1,185 @@
+@file:JvmMultifileClass
+@file:JvmName("XpBundle")
+
+package net.xpece.android.app
+
+import android.annotation.TargetApi
+import android.os.Bundle
+import android.os.IBinder
+import android.os.Parcelable
+import android.support.annotation.RequiresApi
+import android.util.Size
+import android.util.SizeF
+import android.util.SparseArray
+import java.io.Serializable
+import kotlin.reflect.KProperty0
+
+/// BaseBundle
+
+@JvmName("putBoolean")
+fun Bundle.put(property: KProperty0<Boolean>) {
+    putBoolean(property.name, property.get())
+}
+
+@JvmName("putByte")
+fun Bundle.put(property: KProperty0<Byte>) {
+    putByte(property.name, property.get())
+}
+
+@JvmName("putChar")
+fun Bundle.put(property: KProperty0<Char>) {
+    putChar(property.name, property.get())
+}
+
+@JvmName("putShort")
+fun Bundle.put(property: KProperty0<Short>) {
+    putShort(property.name, property.get())
+}
+
+@JvmName("putInt")
+fun Bundle.put(property: KProperty0<Int>) {
+    putInt(property.name, property.get())
+}
+
+@JvmName("putLong")
+fun Bundle.put(property: KProperty0<Long>) {
+    putLong(property.name, property.get())
+}
+
+@JvmName("putFloat")
+fun Bundle.put(property: KProperty0<Float>) {
+    putFloat(property.name, property.get())
+}
+
+@JvmName("putDouble")
+fun Bundle.put(property: KProperty0<Double>) {
+    putDouble(property.name, property.get())
+}
+
+@JvmName("putString")
+fun Bundle.put(property: KProperty0<String>) {
+    putString(property.name, property.get())
+}
+
+@JvmName("putCharSequence")
+fun Bundle.put(property: KProperty0<CharSequence>) {
+    putCharSequence(property.name, property.get())
+}
+
+@JvmName("putIntegerArrayList")
+fun Bundle.put(property: KProperty0<ArrayList<Int>>) {
+    putIntegerArrayList(property.name, property.get())
+}
+
+@JvmName("putStringArrayList")
+fun Bundle.put(property: KProperty0<ArrayList<String>>) {
+    putStringArrayList(property.name, property.get())
+}
+
+@JvmName("putCharSequenceArrayList")
+fun Bundle.put(property: KProperty0<ArrayList<CharSequence>>) {
+    putCharSequenceArrayList(property.name, property.get())
+}
+
+@JvmName("putSerializable")
+fun Bundle.put(property: KProperty0<Serializable>) {
+    putSerializable(property.name, property.get())
+}
+
+@JvmName("putBooleanArray")
+fun Bundle.put(property: KProperty0<BooleanArray>) {
+    putBooleanArray(property.name, property.get())
+}
+
+@JvmName("putByteArray")
+fun Bundle.put(property: KProperty0<ByteArray>) {
+    putByteArray(property.name, property.get())
+}
+
+@JvmName("putCharArray")
+fun Bundle.put(property: KProperty0<CharArray>) {
+    putCharArray(property.name, property.get())
+}
+
+@JvmName("putShortArray")
+fun Bundle.put(property: KProperty0<ShortArray>) {
+    putShortArray(property.name, property.get())
+}
+
+@JvmName("putIntArray")
+fun Bundle.put(property: KProperty0<IntArray>) {
+    putIntArray(property.name, property.get())
+}
+
+@JvmName("putLongArray")
+fun Bundle.put(property: KProperty0<LongArray>) {
+    putLongArray(property.name, property.get())
+}
+
+@JvmName("putFloatArray")
+fun Bundle.put(property: KProperty0<FloatArray>) {
+    putFloatArray(property.name, property.get())
+}
+
+@JvmName("putDoubleArray")
+fun Bundle.put(property: KProperty0<DoubleArray>) {
+    putDoubleArray(property.name, property.get())
+}
+
+@JvmName("putStringArray")
+fun Bundle.put(property: KProperty0<Array<String>>) {
+    putStringArray(property.name, property.get())
+}
+
+@JvmName("putCharSequenceArray")
+fun Bundle.put(property: KProperty0<Array<CharSequence>>) {
+    putCharSequenceArray(property.name, property.get())
+}
+
+/// Bundle
+
+@JvmName("putParcelable")
+fun <T : Parcelable> Bundle.put(property: KProperty0<T>) {
+    putParcelable(property.name, property.get())
+}
+
+@RequiresApi(21)
+@TargetApi(21)
+@JvmName("putSize")
+fun Bundle.put(property: KProperty0<Size>) {
+    putSize(property.name, property.get())
+}
+
+@RequiresApi(21)
+@TargetApi(21)
+@JvmName("putSizeF")
+fun Bundle.put(property: KProperty0<SizeF>) {
+    putSizeF(property.name, property.get())
+}
+
+@JvmName("putParcelableArray")
+fun <T : Parcelable> Bundle.put(property: KProperty0<Array<out T>>) {
+    putParcelableArray(property.name, property.get())
+}
+
+@JvmName("putParcelableArrayList")
+fun <T : Parcelable> Bundle.put(property: KProperty0<ArrayList<out T>>) {
+    putParcelableArrayList(property.name, property.get())
+}
+
+@JvmName("putSparseParcelableArray")
+fun <T : Parcelable> Bundle.put(property: KProperty0<SparseArray<out T>>) {
+    putSparseParcelableArray(property.name, property.get())
+}
+
+@JvmName("putBundle")
+fun Bundle.put(property: KProperty0<Bundle>) {
+    putBundle(property.name, property.get())
+}
+
+@RequiresApi(18)
+@TargetApi(18)
+@JvmName("putBinder")
+fun Bundle.put(property: KProperty0<IBinder>) {
+    putBinder(property.name, property.get())
+}
