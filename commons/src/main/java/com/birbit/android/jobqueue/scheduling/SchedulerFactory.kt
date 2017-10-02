3ab4645865bdb412b99518061ff54ab35d1eb6ff
Eugen Pechanec <e.pechanec@gmail.com>
2017-10-02 14:04:03 +0200

    Fixed typo in ScheudlerFactory

--- commons/src/main/java/com/birbit/android/jobqueue/scheduling/SchedulerFactory.kt
+++ commons/src/main/java/com/birbit/android/jobqueue/scheduling/SchedulerFactory.kt
@@ -1,22 +1,26 @@
 package com.birbit.android.jobqueue.scheduling
 
-import android.annotation.TargetApi
 import android.content.Context
+import android.support.annotation.RequiresApi
 
 /**
  * @author Eugen on 23.08.2016.
  */
 object SchedulerFactory {
+    @JvmStatic
+    @Deprecated("Typo.", ReplaceWith("createSchedulerForGcmJobSchedulerService"))
+    fun createSchedulerForGcmJobSchdulerService(
+            appContext: Context, klass: Class<out GcmJobSchedulerService>): Scheduler =
+            createSchedulerForGcmJobSchedulerService(appContext, klass)
 
     @JvmStatic
-    fun createSchedulerForGcmJobSchdulerService(appContext: Context, klass: Class<out GcmJobSchedulerService>): Scheduler {
-        return GcmJobSchedulerService.createSchedulerFor(appContext, klass)
-    }
+    fun createSchedulerForGcmJobSchedulerService(
+            appContext: Context, klass: Class<out GcmJobSchedulerService>): Scheduler =
+            GcmJobSchedulerService.createSchedulerFor(appContext, klass)
 
     @JvmStatic
-    @TargetApi(21)
-    fun createSchedulerForFrameworkJobSchedulerService(appContext: Context, klass: Class<out FrameworkJobSchedulerService>): Scheduler {
-        return FrameworkJobSchedulerService.createSchedulerFor(appContext, klass)
-    }
-
+    @RequiresApi(21)
+    fun createSchedulerForFrameworkJobSchedulerService(
+            appContext: Context, klass: Class<out FrameworkJobSchedulerService>): Scheduler =
+            FrameworkJobSchedulerService.createSchedulerFor(appContext, klass)
 }
