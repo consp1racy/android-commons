c5d863f9ef4fc188f7a1207f2a8e18a8b5dbe61a
Eugen Pechanec <e.pechanec@gmail.com>
2019-02-10 12:50:07 +0100

    Removed unused conflicting RxJava extension functions

--- commons/src/main/java/io/reactivex/XpReactiveExtensions.kt
+++ commons/src/main/java/io/reactivex/XpReactiveExtensions.kt
@@ -1,35 +1,16 @@
-@file:Suppress("FunctionName", "unused")
+@file:Suppress("FunctionName", "unused", "NOTHING_TO_INLINE")
+@file:JvmName("-XpReactiveExtensions")
 
 package io.reactivex
 
-@JvmSynthetic
-fun <T> Single(callable: () -> T) = Single.fromCallable(callable)!!
+inline fun <T> Single(noinline callable: () -> T): Single<T> = Single.fromCallable(callable)
 
-@JvmSynthetic
-fun Completable(callable: () -> Unit) = Completable.fromCallable(callable)!!
+inline fun Completable(noinline action: () -> Unit): Completable = Completable.fromAction(action)
 
-@JvmSynthetic
-fun <T> Observable(callable: () -> T) = Observable.fromCallable(callable)!!
+inline fun <T> Observable(noinline callable: () -> T): Observable<T> =
+    Observable.fromCallable(callable)
 
-@JvmSynthetic
-fun <T> Maybe(callable: () -> T) = Maybe.fromCallable(callable)!!
+inline fun <T> Maybe(noinline callable: () -> T): Maybe<T> = Maybe.fromCallable(callable)
 
-@JvmSynthetic
-fun <T> Flowable(callable: () -> T) = Flowable.fromCallable(callable)!!
-
-@JvmSynthetic
-fun <T> Single(source: (SingleEmitter<T>) -> Unit) = Single.create(source)!!
-
-@JvmSynthetic
-fun Completable(source: (CompletableEmitter) -> Unit) = Completable.create(source)!!
-
-@JvmSynthetic
-fun <T> Observable(source: (ObservableEmitter<T>) -> Unit) = Observable.create(source)!!
-
-@JvmSynthetic
-fun <T> Maybe(source: (MaybeEmitter<T>) -> Unit) = Maybe.create(source)!!
-
-@JvmSynthetic
-fun <T> Flowable(
-        mode: BackpressureStrategy, source: (FlowableEmitter<T>) -> Unit) =
-        Flowable.create(source, mode)!!
+inline fun <T> Flowable(noinline callable: () -> T): Flowable<T> =
+    Flowable.fromCallable(callable)
