7fc3b8bf66e602f59d2906885d6679ab9522f741
Eugen Pechanec <e.pechanec@gmail.com>
2019-02-10 12:37:41 +0100

    Migrate XpLiveData and XpViewModel out of support library namespace
    This fixes extension functions with Jetifier.

--- commons/src/main/java/android/arch/lifecycle/XpViewModel.kt
+++ commons/src/main/java/android/arch/lifecycle/XpViewModel.kt
@@ -2,45 +2,130 @@ package android.arch.lifecycle
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
+import net.xpece.androidx.lifecycle.activityViewModel as activityViewModelImpl
+import net.xpece.androidx.lifecycle.lazyActivityViewModel as lazyActivityViewModelImpl
+import net.xpece.androidx.lifecycle.lazyViewModel as lazyViewModelImpl
+import net.xpece.androidx.lifecycle.viewModel as viewModelImpl
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "viewModel",
+        imports = ["net.xpece.androidx.lifecycle.viewModel"]
+    )
+)
 inline fun <reified T : ViewModel> FragmentActivity.viewModel() =
-        ViewModelProviders.of(this)[T::class.java]!!
+    viewModelImpl<T>()
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "viewModel",
+        imports = ["net.xpece.androidx.lifecycle.viewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.viewModel() =
-        ViewModelProviders.of(this)[T::class.java]!!
+    viewModelImpl<T>()
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "activityViewModel",
+        imports = ["net.xpece.androidx.lifecycle.activityViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.activityViewModel() =
-        ViewModelProviders.of(activity!!)[T::class.java]!!
+    activityViewModelImpl<T>()
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "viewModel",
+        imports = ["net.xpece.androidx.lifecycle.viewModel"]
+    )
+)
 inline fun <reified T : ViewModel> FragmentActivity.viewModel(factory: ViewModelProvider.Factory) =
-        ViewModelProviders.of(this, factory)[T::class.java]!!
+    viewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "viewModel",
+        imports = ["net.xpece.androidx.lifecycle.viewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.viewModel(factory: ViewModelProvider.Factory) =
-        ViewModelProviders.of(this, factory)[T::class.java]!!
+    viewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "activityViewModel",
+        imports = ["net.xpece.androidx.lifecycle.activityViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.activityViewModel(factory: ViewModelProvider.Factory) =
-        ViewModelProviders.of(activity!!, factory)[T::class.java]!!
+    activityViewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> FragmentActivity.lazyViewModel(
-        crossinline factory: () -> ViewModelProvider.Factory) = lazy(LazyThreadSafetyMode.NONE) {
-    viewModel<T>(factory())
-}
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazyViewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> FragmentActivity.lazyViewModel() =
-        lazy(LazyThreadSafetyMode.NONE) { viewModel<T>() }
+    lazyViewModelImpl<T>()
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.lazyViewModel(
-        crossinline factory: () -> ViewModelProvider.Factory) = lazy(LazyThreadSafetyMode.NONE) {
-    viewModel<T>(factory())
-}
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazyViewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.lazyViewModel() =
-        lazy(LazyThreadSafetyMode.NONE) { viewModel<T>() }
+    lazyViewModelImpl<T>()
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyActivityViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyActivityViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.lazyActivityViewModel(
-        crossinline factory: () -> ViewModelProvider.Factory) = lazy(LazyThreadSafetyMode.NONE) {
-    activityViewModel<T>(factory())
-}
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazyActivityViewModelImpl<T>(factory)
 
+@Deprecated(
+    "This extension function is not available when using Jetifier.",
+    ReplaceWith(
+        "lazyActivityViewModel",
+        imports = ["net.xpece.androidx.lifecycle.lazyActivityViewModel"]
+    )
+)
 inline fun <reified T : ViewModel> Fragment.lazyActivityViewModel() =
-        lazy(LazyThreadSafetyMode.NONE) { activityViewModel<T>() }
+    lazyActivityViewModelImpl<T>()
---
+++ commons/src/main/java/net/xpece/androidx/lifecycle/XpViewModel.kt
@@ -0,0 +1,54 @@
+@file:Suppress("unused")
+
+package net.xpece.androidx.lifecycle
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentActivity
+
+inline fun <reified T : ViewModel> FragmentActivity.viewModel() =
+    ViewModelProviders.of(this)[T::class.java]
+
+inline fun <reified T : ViewModel> Fragment.viewModel() =
+    ViewModelProviders.of(this)[T::class.java]
+
+inline fun <reified T : ViewModel> Fragment.activityViewModel() =
+    ViewModelProviders.of(activity!!)[T::class.java]
+
+inline fun <reified T : ViewModel> FragmentActivity.viewModel(factory: ViewModelProvider.Factory) =
+    ViewModelProviders.of(this, factory)[T::class.java]
+
+inline fun <reified T : ViewModel> Fragment.viewModel(factory: ViewModelProvider.Factory) =
+    ViewModelProviders.of(this, factory)[T::class.java]
+
+inline fun <reified T : ViewModel> Fragment.activityViewModel(factory: ViewModelProvider.Factory) =
+    ViewModelProviders.of(activity!!, factory)[T::class.java]
+
+inline fun <reified T : ViewModel> FragmentActivity.lazyViewModel(
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazy(LazyThreadSafetyMode.NONE) {
+    viewModel<T>(factory())
+}
+
+inline fun <reified T : ViewModel> FragmentActivity.lazyViewModel() =
+    lazy(LazyThreadSafetyMode.NONE) { viewModel<T>() }
+
+inline fun <reified T : ViewModel> Fragment.lazyViewModel(
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazy(LazyThreadSafetyMode.NONE) {
+    viewModel<T>(factory())
+}
+
+inline fun <reified T : ViewModel> Fragment.lazyViewModel() =
+    lazy(LazyThreadSafetyMode.NONE) { viewModel<T>() }
+
+inline fun <reified T : ViewModel> Fragment.lazyActivityViewModel(
+    crossinline factory: () -> ViewModelProvider.Factory
+) = lazy(LazyThreadSafetyMode.NONE) {
+    activityViewModel<T>(factory())
+}
+
+inline fun <reified T : ViewModel> Fragment.lazyActivityViewModel() =
+    lazy(LazyThreadSafetyMode.NONE) { activityViewModel<T>() }
