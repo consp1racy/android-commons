a25e7c08c5bb2319eba28df723455edb9bc15f20
Eugen Pechanec <e.pechanec@gmail.com>
2019-02-10 14:34:38 +0100

    Delegated XpHandler to AndroidX extensions

--- commons/src/main/java/net/xpece/android/os/XpHandler.kt
+++ commons/src/main/java/net/xpece/android/os/XpHandler.kt
@@ -1,31 +1,79 @@
+@file:JvmName("-XpHandler")
+
 package net.xpece.android.os
 
 import android.os.Handler
 import android.os.SystemClock
 
-@JvmSynthetic
+@Deprecated(
+    "Reorder parameters. Use Long. Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis.toLong(), r)",
+        imports = ["androidx.core.os.postDelayed"]
+    )
+)
 fun Handler.postDelayed(r: Runnable, delayMillis: Int) =
-        postDelayed(r, delayMillis.toLong())
+    postDelayed(r, delayMillis.toLong())
 
-@JvmSynthetic
+@Deprecated(
+    "Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis.toLong(), runnable)",
+        imports = ["androidx.core.os.postDelayed"]
+    )
+)
 fun Handler.postDelayed(delayMillis: Int, runnable: () -> Unit) =
-        this.postDelayed(runnable, delayMillis.toLong())
+    this.postDelayed(runnable, delayMillis.toLong())
 
-@JvmSynthetic
+@Deprecated(
+    "Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis, runnable)",
+        imports = ["androidx.core.os.postDelayed"]
+    )
+)
 fun Handler.postDelayed(delayMillis: Long, runnable: () -> Unit) =
-        this.postDelayed(runnable, delayMillis)
+    this.postDelayed(runnable, delayMillis)
 
-@JvmSynthetic
+@Deprecated(
+    "Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis.toLong(), token, runnable)",
+        imports = ["androidx.core.os.postDelayed"]
+    )
+)
 fun Handler.postDelayed(token: Any, delayMillis: Int, r: () -> Unit) =
-        postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
+    postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
 
-@JvmSynthetic
+@Deprecated(
+    "Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis, token, runnable)",
+        imports = ["androidx.core.os.postDelayed"]
+    )
+)
 fun Handler.postDelayed(token: Any, delayMillis: Long, r: () -> Unit) =
-        postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
+    postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
 
-@JvmSynthetic
+@Deprecated(
+    "Declared by SDK API 28. Reorder parameters. Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis.toLong(), token, r)",
+        imports = ["androidx.core.os.postDelayed"]
+    ),
+    DeprecationLevel.HIDDEN
+)
 fun Handler.postDelayed(r: Runnable, token: Any, delayMillis: Int) =
-        postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
+    postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
 
+@Deprecated(
+    "Declared by SDK API 28. Reorder parameters. Use AndroidX.",
+    ReplaceWith(
+        "postDelayed(delayMillis.toLong(), token, r)",
+        imports = ["androidx.core.os.postDelayed"]
+    ),
+    DeprecationLevel.HIDDEN
+)
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 fun Handler.postDelayed(r: Runnable, token: Any, delayMillis: Long) =
-        postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
+    postAtTime(r, token, SystemClock.uptimeMillis() + delayMillis)
