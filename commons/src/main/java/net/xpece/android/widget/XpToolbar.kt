eb216c9f2a86eda5e0d76bcd24c4f935eab6d517
Eugen Pechanec <e.pechanec@gmail.com>
2019-02-10 13:30:41 +0100

    XpToolbar can now access and modify titleTextColor and subtitleTextColor

--- commons/src/main/java/net/xpece/android/widget/XpToolbar.kt
+++ commons/src/main/java/net/xpece/android/widget/XpToolbar.kt
@@ -1,9 +1,11 @@
 @file:JvmName("XpToolbar")
+@file:Suppress("unused")
 
 package net.xpece.android.widget
 
 import android.annotation.SuppressLint
 import android.graphics.drawable.Drawable
+import android.support.annotation.ColorInt
 import android.support.annotation.DrawableRes
 import android.support.annotation.StyleRes
 import android.support.v7.widget.AppCompatDrawableManager
@@ -12,18 +14,29 @@ import android.widget.TextView
 
 private val CLASS_TOOLBAR = Toolbar::class.java
 
-private val FIELD_COLLAPSE_ICON = CLASS_TOOLBAR.getDeclaredField(
-        "mCollapseIcon")!!.apply { isAccessible = true }
+private val FIELD_COLLAPSE_ICON by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mCollapseIcon").apply { isAccessible = true }
+}
 
-private val FIELD_TITLE_TEXT_VIEW = CLASS_TOOLBAR.getDeclaredField(
-        "mTitleTextView")!!.apply { isAccessible = true }
-private val FIELD_TITLE_TEXT_APPEARANCE = CLASS_TOOLBAR.getDeclaredField(
-        "mTitleTextAppearance")!!.apply { isAccessible = true }
+private val FIELD_TITLE_TEXT_VIEW by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mTitleTextView").apply { isAccessible = true }
+}
+private val FIELD_TITLE_TEXT_APPEARANCE by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mTitleTextAppearance").apply { isAccessible = true }
+}
+private val FIELD_TITLE_TEXT_COLOR by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mTitleTextColor").apply { isAccessible = true }
+}
 
-private val FIELD_SUBTITLE_TEXT_VIEW = CLASS_TOOLBAR.getDeclaredField(
-        "mSubtitleTextView")!!.apply { isAccessible = true }
-private val FIELD_SUBTITLE_TEXT_APPEARANCE = CLASS_TOOLBAR.getDeclaredField(
-        "mSubtitleTextAppearance")!!.apply { isAccessible = true }
+private val FIELD_SUBTITLE_TEXT_VIEW by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mSubtitleTextView").apply { isAccessible = true }
+}
+private val FIELD_SUBTITLE_TEXT_APPEARANCE by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mSubtitleTextAppearance").apply { isAccessible = true }
+}
+private val FIELD_SUBTITLE_TEXT_COLOR by lazy(LazyThreadSafetyMode.NONE) {
+    CLASS_TOOLBAR.getDeclaredField("mSubtitleTextColor").apply { isAccessible = true }
+}
 
 var Toolbar.collapseIcon: Drawable?
     get() = FIELD_COLLAPSE_ICON.get(this) as Drawable?
@@ -43,6 +56,12 @@ var Toolbar.titleTextAppearance: Int
         setTitleTextAppearance(context, value)
     }
 
+var Toolbar.titleTextColor: Int
+    @ColorInt get() = FIELD_TITLE_TEXT_COLOR.getInt(this)
+    set(@ColorInt value) {
+        setTitleTextColor(value)
+    }
+
 val Toolbar.subtitleTextView: TextView?
     get() = FIELD_SUBTITLE_TEXT_VIEW.get(this) as TextView?
 
@@ -50,4 +69,10 @@ var Toolbar.subtitleTextAppearance: Int
     @StyleRes get() = FIELD_SUBTITLE_TEXT_APPEARANCE.getInt(this)
     set(@StyleRes value) {
         setSubtitleTextAppearance(context, value)
+    }
+
+var Toolbar.subtitleTextColor: Int
+    @ColorInt get() = FIELD_SUBTITLE_TEXT_COLOR.getInt(this)
+    set(@ColorInt value) {
+        setSubtitleTextColor(value)
     }
