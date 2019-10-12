3d4a6bfe9332ea473a0f79ed811c526cc08281de
Eugen Pechanec <e.pechanec@gmail.com>
2019-10-12 16:54:47 +0200

    Make XpCursor more efficient

--- commons/src/main/java/net/xpece/android/database/XpCursor.kt
+++ commons/src/main/java/net/xpece/android/database/XpCursor.kt
@@ -4,15 +4,21 @@
 package net.xpece.android.database
 
 import android.database.Cursor
-import androidx.core.database.getIntOrNull
-import androidx.core.database.getLongOrNull
-import androidx.core.database.getStringOrNull
 
-inline fun Cursor.getLong(index: Int, fallback: Long): Long =
-    getLongOrNull(index) ?: fallback
+inline fun Cursor.getDouble(index: Int, fallback: Double): Double =
+        if (!isNull(index)) getDouble(index) else fallback
+
+inline fun Cursor.getFloat(index: Int, fallback: Float): Float =
+        if (!isNull(index)) getFloat(index) else fallback
 
 inline fun Cursor.getInt(index: Int, fallback: Int): Int =
-    getIntOrNull(index) ?: fallback
+        if (!isNull(index)) getInt(index) else fallback
+
+inline fun Cursor.getLong(index: Int, fallback: Long): Long =
+        if (!isNull(index)) getLong(index) else fallback
+
+inline fun Cursor.getShort(index: Int, fallback: Short): Short =
+        if (!isNull(index)) getShort(index) else fallback
 
 inline fun Cursor.getString(index: Int, fallback: String): String =
-    getStringOrNull(index) ?: fallback
+        if (!isNull(index)) getString(index) else fallback
