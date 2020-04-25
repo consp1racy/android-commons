644aa48b74ef404c7671b675126f9563d740307a
Eugen Pechanec <e.pechanec@gmail.com>
2020-04-25 13:36:43 +0200

    Update LiveData extensions
    Deprecate MutableLiveData.setValueAndNull in favor of LiveEvent library.
    Remove nonsensical LiveData.observe extension function.
    Rename observeFilterNotNull to observeNonNull.

--- commons/src/main/java/net/xpece/androidx/lifecycle/-LiveData.kt
+++ commons/src/main/java/net/xpece/androidx/lifecycle/-LiveData.kt
@@ -10,23 +10,46 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 
 inline fun <T> LiveData<T>.observeNullable(
-    owner: LifecycleOwner, crossinline observer: (T?) -> Unit
-) = observe(owner, Observer { observer(it) })
+    owner: LifecycleOwner,
+    crossinline observer: (T?) -> Unit
+): Observer<T> {
+    val observer1 = Observer<T> { observer(it) }
+    observe(owner, observer1)
+    return observer1
+}
 
+@Deprecated(
+    message = "Renamed.",
+    replaceWith = ReplaceWith("observeNonNull(owner, observer)", "net.xpece.androidx.lifecycle.observeNonNull")
+)
 inline fun <T> LiveData<T>.observeFilterNotNull(
-    owner: LifecycleOwner, crossinline observer: (T) -> Unit
-) = observe(owner, Observer { it?.let(observer) })
+    owner: LifecycleOwner,
+    crossinline observer: (T) -> Unit
+) = observeNonNull(owner, observer)
 
-inline fun <T> LiveData<T>.observe(
-    owner: LifecycleOwner, crossinline observer: (T) -> Unit, crossinline ifNull: () -> Unit
-) = observe(owner, Observer { it?.let(observer) ?: ifNull() })
+inline fun <T> LiveData<T>.observeNonNull(
+    owner: LifecycleOwner,
+    crossinline observer: (T) -> Unit
+): Observer<T> {
+    val observer1 = Observer<T> { it?.let(observer) }
+    observe(owner, observer1)
+    return observer1
+}
 
+/**
+ * See [https://github.com/hadilq/LiveEvent].
+ */
+@Deprecated("Consider using LiveEvent library instead.")
 @Suppress("NOTHING_TO_INLINE")
 @MainThread
 inline fun <T> MutableLiveData<T>.setValueAndNull(value: T) {
     setValue(value); setValue(null)
 }
 
+/**
+ * See [https://github.com/hadilq/LiveEvent].
+ */
+@Deprecated("Consider using LiveEvent library instead.")
 @Suppress("NOTHING_TO_INLINE")
 inline fun <T> MutableLiveData<T>.postValueAndNull(value: T) {
     postValue(value); postValue(null)
