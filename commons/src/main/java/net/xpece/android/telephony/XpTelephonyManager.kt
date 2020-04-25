c5fc0c11ce76dd03ad1efcd4b625b4a6d0b1adaf
Eugen Pechanec <e.pechanec@gmail.com>
2020-04-25 13:43:02 +0200

    Remove superfluous TelephonyManager extension functions

--- commons/src/main/java/net/xpece/android/telephony/XpTelephonyManager.kt
+++ commons/src/main/java/net/xpece/android/telephony/XpTelephonyManager.kt
@@ -27,19 +27,3 @@ val TelephonyManager.isDataEnabledCompat: Boolean
     } else {
         TelephonyManagerReflection.methodGetDataEnabled.invoke(this) as Boolean
     }
-
-inline val TelephonyManager.isPhone: Boolean
-    get() = phoneType != TelephonyManager.PHONE_TYPE_NONE
-
-inline val TelephonyManager.isSimStateReady: Boolean
-    get() = simState == TelephonyManager.SIM_STATE_READY
-
-inline val TelephonyManager.isCallStateIdle: Boolean
-    get() = callState == TelephonyManager.CALL_STATE_IDLE
-
-inline val TelephonyManager.isVoiceCapableCompat: Boolean
-    get() = Build.VERSION.SDK_INT < 22 || isVoiceCapable
-
-@Deprecated("This method provides no meaningful value. Determine state based on your use case.")
-inline val TelephonyManager.isCallCapableInstantly: Boolean
-    get() = isVoiceCapableCompat && isPhone && isSimStateReady && isCallStateIdle
