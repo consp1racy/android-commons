e3415a89ab1c855875f26bdfd5ceb9996f7985c5
Eugen Pechanec <e.pechanec@gmail.com>
2020-04-25 17:32:07 +0200

    Update XpParcel

--- commons/src/main/java/net/xpece/android/os/XpParcel.kt
+++ commons/src/main/java/net/xpece/android/os/XpParcel.kt
@@ -1,38 +1,36 @@
 @file:JvmName("XpParcel")
+@file:Suppress("NOTHING_TO_INLINE")
 
 package net.xpece.android.os
 
 import android.annotation.SuppressLint
 import android.os.Parcel
 
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 @Deprecated(
-        "Shadows method since API 29.",
-        replaceWith = ReplaceWith("writeBooleanCompat"),
-        level = DeprecationLevel.HIDDEN
+    "Shadows method since API 29.",
+    replaceWith = ReplaceWith("writeBooleanCompat"),
+    level = DeprecationLevel.HIDDEN
 )
 inline fun Parcel.writeBoolean(value: Boolean) = writeBooleanCompat(value)
 
-@Suppress("NOTHING_TO_INLINE", "EXTENSION_SHADOWED_BY_MEMBER")
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
 @Deprecated(
-        "Shadows method since API 29.",
-        replaceWith = ReplaceWith("readBooleanCompat"),
-        level = DeprecationLevel.HIDDEN
+    "Shadows method since API 29.",
+    replaceWith = ReplaceWith("readBooleanCompat"),
+    level = DeprecationLevel.HIDDEN
 )
 inline fun Parcel.readBoolean(): Boolean = readBooleanCompat()
 
-@Suppress("NOTHING_TO_INLINE")
 inline fun Parcel.writeBooleanCompat(value: Boolean) {
     writeInt(if (value) 1 else 0)
 }
 
-@Suppress("NOTHING_TO_INLINE")
 inline fun Parcel.readBooleanCompat(): Boolean = readInt() != 0
 
 /**
  * Writes `null` as [Float.NaN]. Only uses 4 bytes.
  */
-@Suppress("NOTHING_TO_INLINE")
 inline fun Parcel.writeFloatOrNan(value: Float?) {
     writeFloat(value ?: Float.NaN)
 }
@@ -41,13 +39,11 @@ inline fun Parcel.writeFloatOrNan(value: Float?) {
  * Reads [Float.NaN] as `null`.
  */
 @SuppressLint("ParcelClassLoader")
-@Suppress("NOTHING_TO_INLINE")
-inline fun Parcel.readFloatOrNull(): Float? = readFloat().takeIf { !it.isNaN() }
+inline fun Parcel.readFloatOrNull(): Float? = readFloat().takeUnless(Float::isNaN)
 
 /**
- * Writes `null` as [Double.NaN]. Only uses 4 bytes.
+ * Writes `null` as [Double.NaN]. Only uses 8 bytes.
  */
-@Suppress("NOTHING_TO_INLINE")
 inline fun Parcel.writeDoubleOrNan(value: Double?) {
     writeDouble(value ?: Double.NaN)
 }
@@ -56,13 +52,23 @@ inline fun Parcel.writeDoubleOrNan(value: Double?) {
  * Reads [Double.NaN] as `null`.
  */
 @SuppressLint("ParcelClassLoader")
-@Suppress("NOTHING_TO_INLINE")
-inline fun Parcel.readDoubleOrNull(): Double? = readDouble().takeIf { !it.isNaN() }
+inline fun Parcel.readDoubleOrNull(): Double? = readDouble().takeUnless(Double::isNaN)
 
 inline fun <reified E> Parcel.readList(cl: ClassLoader? = E::class.java.classLoader): List<E> =
-        mutableListOf<E>().apply {
-            readList(this as List<*>, cl)
-        }
+    mutableListOf<E>().apply {
+        readList(this as List<*>, cl)
+    }
 
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+@Deprecated(
+    "Renamed.",
+    replaceWith = ReplaceWith("readValue", "net.xpece.android.os.readValue"),
+    level = DeprecationLevel.WARNING
+)
 inline fun <reified T> Parcel.readTypedValue(cl: ClassLoader? = T::class.java.classLoader): T =
-        readValue(cl) as T
+    readValue(cl) as T
+
+// Name is same but parameters differ.
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+inline fun <reified T> Parcel.readValue(cl: ClassLoader? = T::class.java.classLoader): T =
+    readValue(cl) as T
