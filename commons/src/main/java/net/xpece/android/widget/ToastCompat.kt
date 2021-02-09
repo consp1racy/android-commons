8ae66b57b47caf6ac9fc184b3d315ebd87f4bdb0
Eugen Pechanec <e.pechanec@gmail.com>
2021-02-09 23:00:07 +0100

    Migrate ToastCompat to Kotlin - Kotlin source incompatible change

--- commons/src/main/java/net/xpece/android/widget/ToastCompat.kt
+++ commons/src/main/java/net/xpece/android/widget/ToastCompat.kt
@@ -1,133 +1,102 @@
-package net.xpece.android.widget;
+@file:JvmName("ToastCompat")
+@file:Suppress("FunctionName")
 
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.res.Resources;
-import android.os.Build;
-import androidx.annotation.IntDef;
-import androidx.annotation.StringRes;
-import android.view.Display;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
-import android.widget.Toast;
+package net.xpece.android.widget
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.res.Resources
+import android.os.Build.VERSION.SDK_INT
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.WindowManager.BadTokenException
+import android.widget.Toast
+import androidx.annotation.CheckResult
+import androidx.annotation.IntDef
+import androidx.annotation.StringRes
+import kotlin.annotation.AnnotationRetention.SOURCE
+import android.widget.Toast as ToastImpl
 
+/**
+ * Construct an empty Toast object. You must call [setView][Toast.setView] before you
+ * can call [show][Toast.show].
+ *
+ * @param context The context to use. Usually your [Application] or [Activity] object.
+ */
+@CheckResult
+@JvmName("make")
+fun Toast(context: Context): Toast {
+    return ToastImpl(context.forSafeToast())
+}
 
-public class ToastCompat {
+/**
+ * Make a standard toast that just contains a text view.
+ *
+ * @param context The context to use. Usually your [Application] or [Activity] object.
+ * @param text The text to show. Can be formatted text.
+ * @param duration How long to display the message. Either [Toast.LENGTH_SHORT] or
+ * [Toast.LENGTH_LONG]
+ */
+@CheckResult
+@JvmName("makeText")
+fun ToastCompat(context: Context, text: CharSequence, @Duration duration: Int): Toast {
+    return Toast.makeText(context.forSafeToast(), text, duration)
+}
 
-    @IntDef({Toast.LENGTH_SHORT, Toast.LENGTH_LONG})
-    @Retention(RetentionPolicy.SOURCE)
-    private @interface Duration {}
+/**
+ * Make a standard toast that just contains a text view with the text from a resource.
+ *
+ * @param context The context to use. Usually your [Application] or [Activity] object.
+ * @param resId The resource id of the string resource to use. Can be formatted text.
+ * @param duration How long to display the message. Either [Toast.LENGTH_SHORT] or
+ * [Toast.LENGTH_LONG]
+ * @throws Resources.NotFoundException if the resource can't be found.
+ */
+@CheckResult
+@JvmName("makeText")
+fun ToastCompat(context: Context, @StringRes resId: Int, @Duration duration: Int): Toast {
+    return Toast.makeText(context.forSafeToast(), resId, duration)
+}
 
-    /**
-     * Make a standard toast that just contains a text view.
-     *
-     * @param context The context to use.  Usually your {@link android.app.Application}
-     * or {@link android.app.Activity} object.
-     * @param text The text to show.  Can be formatted text.
-     * @param duration How long to display the message.  Either {@link Toast#LENGTH_SHORT} or
-     * {@link Toast#LENGTH_LONG}
-     */
-    public static Toast makeText(Context context, CharSequence text, @Duration int duration) {
-        return Toast.makeText(wrap(context), text, duration);
-    }
+@IntDef(Toast.LENGTH_SHORT, Toast.LENGTH_LONG)
+@Retention(SOURCE)
+private annotation class Duration
 
-    /**
-     * Make a standard toast that just contains a text view with the text from a resource.
-     *
-     * @param context The context to use.  Usually your {@link android.app.Application}
-     * or {@link android.app.Activity} object.
-     * @param resId The resource id of the string resource to use.  Can be formatted text.
-     * @param duration How long to display the message.  Either {@link Toast#LENGTH_SHORT} or
-     * {@link Toast#LENGTH_LONG}
-     * @throws Resources.NotFoundException if the resource can't be found.
-     */
-    public static Toast makeText(Context context, @StringRes int resId, @Duration int duration)
-            throws Resources.NotFoundException {
-        return Toast.makeText(wrap(context), context.getResources().getText(resId), duration);
-    }
+private fun Context.forSafeToast(): Context = when (SDK_INT) {
+    25 -> SafeToastContext(this)
+    else -> this
+}
 
-    private static Context wrap(final Context context) {
-        if (Build.VERSION.SDK_INT == 25) {
-            return new FixedContext(context);
-        } else {
-            return context;
+private class SafeToastContext(base: Context) : ContextWrapper(base) {
+
+    override fun getSystemService(name: String): Any = when (name) {
+        WINDOW_SERVICE -> {
+            val impl = super.getSystemService(name) as WindowManager
+            SafeToastWindowManager(impl)
+        }
+        else -> {
+            super.getSystemService(name)
         }
     }
 
-    private static class FixedContext extends ContextWrapper {
-        public FixedContext(final Context base) {
-            super(base);
-        }
-
-        @Override
-        public Object getSystemService(final String name) {
-            if (Context.WINDOW_SERVICE.equals(name)) {
-                return new FixedWindowManager((WindowManager) super.getSystemService(Context.WINDOW_SERVICE), null);
-            } else {
-                return super.getSystemService(name);
-            }
-        }
-
-        @Override
-        public Context getApplicationContext() {
-            return new FixedContext(super.getApplicationContext());
-        }
+    override fun getApplicationContext(): Context {
+        return SafeToastContext(super.getApplicationContext())
     }
+}
 
-    private static class FixedWindowManager extends WindowManagerWrapper {
-        private final Runnable mOnBadTokenException;
+private class SafeToastWindowManager(
+    private val base: WindowManager,
+    private val onBadTokenException: Runnable? = null
+) : WindowManager by base {
 
-        FixedWindowManager(final WindowManager impl, final Runnable onBadTokenException) {
-            super(impl);
-            mOnBadTokenException = onBadTokenException;
-        }
-
-        @Override
-        public void addView(final View view, final ViewGroup.LayoutParams params) {
-            try {
-                super.addView(view, params);
-            } catch (BadTokenException ex) {
-                if (mOnBadTokenException != null) {
-                    mOnBadTokenException.run();
-                }
-            }
-        }
-    }
-
-    private static class WindowManagerWrapper implements WindowManager {
-        private final WindowManager mBase;
-
-        WindowManagerWrapper(final WindowManager base) {
-            mBase = base;
-        }
-
-        @Override
-        public Display getDefaultDisplay() {
-            return mBase.getDefaultDisplay();
-        }
-
-        @Override
-        public void removeViewImmediate(final View view) {
-            mBase.removeViewImmediate(view);
-        }
-
-        @Override
-        public void addView(final View view, final ViewGroup.LayoutParams params) {
-            mBase.addView(view, params);
-        }
-
-        @Override
-        public void updateViewLayout(final View view, final ViewGroup.LayoutParams params) {
-            mBase.updateViewLayout(view, params);
-        }
-
-        @Override
-        public void removeView(final View view) {
-            mBase.removeView(view);
+    override fun addView(view: View, params: ViewGroup.LayoutParams) {
+        try {
+            base.addView(view, params)
+        } catch (ex: BadTokenException) {
+            onBadTokenException?.run()
         }
     }
 }
