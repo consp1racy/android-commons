605857715cad9b40aaad438fed703937358f9d27
Eugen Pechanec <e.pechanec@gmail.com>
2021-02-09 21:37:07 +0100

    Remove publishing to Bintray

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
-        classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
     }
 }
 
