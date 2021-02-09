fbf3956516579c5c0acd8fe603059c450ba7874d
Eugen Pechanec <e.pechanec@gmail.com>
2021-02-09 19:49:32 +0100

    Fix multiple medium spans in a single TextView

--- text-span/src/main/java/net/xpece/android/text/span/MediumSpanCompat.kt
+++ text-span/src/main/java/net/xpece/android/text/span/MediumSpanCompat.kt
@@ -1,20 +1,40 @@
 @file:JvmName("MediumSpanCompat")
+@file:Suppress("FunctionName")
 
 package net.xpece.android.text.span
 
 import android.graphics.Typeface
-import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.text.style.StyleSpan
 import android.text.style.TypefaceSpan
 
-private val mediumSpanCompatImpl: Any = run {
-    if (Build.VERSION.SDK_INT >= 21) {
-        TypefaceSpan("sans-serif-medium")
-    } else {
-        StyleSpan(Typeface.BOLD)
-    }
+private val FACTORY = when {
+    SDK_INT >= 21 -> RealMediumSpanFactory
+    else -> FakeMediumSpanFactory
 }
 
+@Deprecated(
+    message = "Every span must be a unique instance.",
+    replaceWith = ReplaceWith("MediumSpanCompat()"),
+    level = DeprecationLevel.ERROR
+)
 @get:JvmName("getInstance")
-val MediumSpanCompat: Any
-    get() = mediumSpanCompatImpl
+val MediumSpanCompat: Any = FACTORY.create()
+
+@JvmName("create")
+fun MediumSpanCompat(): Any = FACTORY.create()
+
+interface MediumSpanFactory {
+
+    fun create(): Any
+}
+
+object FakeMediumSpanFactory : MediumSpanFactory {
+
+    override fun create(): Any = StyleSpan(Typeface.BOLD)
+}
+
+object RealMediumSpanFactory : MediumSpanFactory {
+
+    override fun create(): Any = TypefaceSpan("sans-serif-medium")
+}
