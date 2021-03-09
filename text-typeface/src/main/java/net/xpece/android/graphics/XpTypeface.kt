369ef19fe8bd41759a32fd9dfdfa8fe0d0b60df0
Eugen Pechanec <e.pechanec@gmail.com>
2021-03-09 12:24:22 +0100

    `@JvmOverloads XpTypeface.create`

--- text-typeface/src/main/java/net/xpece/android/graphics/XpTypeface.kt
+++ text-typeface/src/main/java/net/xpece/android/graphics/XpTypeface.kt
@@ -4,20 +4,22 @@ package net.xpece.android.graphics
 
 import android.content.Context
 import android.graphics.Typeface
-import android.os.Build
+import android.os.Build.VERSION.SDK_INT
 import android.util.Log
 import androidx.annotation.IntRange
 import androidx.annotation.RequiresApi
 
 @JvmName("create")
+@JvmOverloads
 fun Typeface.withExactStyle(
     context: Context,
     weight: Int,
     italic: Boolean = isItalic
 ): Typeface = createInternal(context, this, weight, italic)
 
-@JvmName("create")
 @RequiresApi(21)
+@JvmName("create")
+@JvmOverloads
 fun Typeface.withExactStyle(
     weight: Int,
     italic: Boolean = isItalic
@@ -30,13 +32,13 @@ private fun createInternal(
     italic: Boolean
 ): Typeface = try {
     when {
-        Build.VERSION.SDK_INT >= 28 -> {
+        SDK_INT >= 28 -> {
             Typeface.create(family, weight, italic)
         }
-        Build.VERSION.SDK_INT >= 26 -> {
+        SDK_INT >= 26 -> {
             WeightTypefaceOreo.create(family, weight, italic)
         }
-        Build.VERSION.SDK_INT >= 21 -> {
+        SDK_INT >= 21 -> {
             WeightTypefaceLollipop.create(family, weight, italic)
         }
         else -> {
