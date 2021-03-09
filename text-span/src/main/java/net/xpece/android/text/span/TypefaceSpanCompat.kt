2ff8ccdbf667a9ee7994e81edf16677084f260ce
Eugen Pechanec <e.pechanec@gmail.com>
2021-03-09 03:25:15 +0100

    Rename Typeface.span to Typeface.asSpan

--- text-span/src/main/java/net/xpece/android/text/span/TypefaceSpanCompat.kt
+++ text-span/src/main/java/net/xpece/android/text/span/TypefaceSpanCompat.kt
@@ -7,10 +7,20 @@ import android.os.Build.VERSION.SDK_INT
 import android.text.style.TypefaceSpan
 
 @JvmName("create")
-fun Typeface.span(): TypefaceSpan {
+fun Typeface.asSpan(): TypefaceSpan {
     return if (SDK_INT >= 28) {
         TypefaceSpan(this)
     } else {
         TypefaceSpanCompatImpl(this)
     }
+}
+
+@Deprecated(
+    message = "Renamed.",
+    replaceWith = ReplaceWith("asSpan()", "net.xpece.android.text.span.asSpan"),
+    level = DeprecationLevel.ERROR
+)
+@JvmSynthetic
+fun Typeface.span(): TypefaceSpan {
+    return asSpan()
 }
