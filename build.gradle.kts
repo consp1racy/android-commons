21182673de82e60676a093a9b380f0642d66d913
Eugen Pechanec <e.pechanec@gmail.com>
2021-03-10 20:08:48 +0100

    Add Kotlin binary compatibility validator plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:4.1.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
+        classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.4.0")
     }
 
     repositories {
@@ -12,6 +13,15 @@ buildscript {
         mavenCentral()
         jcenter()
     }
+}
+
+apply(plugin = "binary-compatibility-validator")
+
+extensions.configure<kotlinx.validation.ApiValidationExtension> {
+    // Ignore all sample projects, since they're not part of our API.
+    // Only leaf project name is valid configuration, and every project must be individually ignored.
+    // See https://github.com/Kotlin/binary-compatibility-validator/issues/3
+    ignoredProjects.addAll(listOf("sample-cardbutton", "sample-commons", "sample-extra"))
 }
 
 subprojects {
