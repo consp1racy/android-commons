108b4bc87ab770c7846f66509461574aad3fb4d7
Eugen Pechanec <e.pechanec@gmail.com>
2021-03-11 10:19:11 +0100

    Don't publish commons:commons to Maven Central

--- commons/commons/build.gradle.kts
+++ commons/commons/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("net.xpece.android")
-    id("net.xpece.publish.sonatype")
 }
 
 group = rootProject.property("GROUP_ID") as String
