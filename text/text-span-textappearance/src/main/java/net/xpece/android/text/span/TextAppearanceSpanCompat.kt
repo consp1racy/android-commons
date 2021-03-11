dbca548c8bd045eae63be5d2069c5030e2741031
Eugen Pechanec <e.pechanec@gmail.com>
2021-03-11 15:10:29 +0100

    Add javadoc to text-span-textappearance

--- text/text-span-textappearance/src/main/java/net/xpece/android/text/span/TextAppearanceSpanCompat.kt
+++ text/text-span-textappearance/src/main/java/net/xpece/android/text/span/TextAppearanceSpanCompat.kt
@@ -8,6 +8,29 @@ import android.os.Build.VERSION.SDK_INT
 import android.text.style.TextAppearanceSpan
 import androidx.annotation.StyleRes
 
+/**
+ * Sets the text appearance using the given `TextAppearanceSpanCompat` attributes.
+ * By default `TextAppearanceSpanCompat` only changes the specified attributes in XML.
+ * `textColorHighlight`, `textColorHint`, `textAllCaps` and `fallbackLineSpacing`
+ * are not supported by `TextAppearanceSpanCompat`.
+ *
+ * @see android.widget.TextView.setTextAppearance
+ * @see android.R.attr.fontFamily
+ * @see android.R.attr.textColor
+ * @see android.R.attr.textColorLink
+ * @see android.R.attr.textFontWeight
+ * @see android.R.attr.textSize
+ * @see android.R.attr.textStyle
+ * @see android.R.attr.typeface
+ * @see android.R.attr.shadowColor
+ * @see android.R.attr.shadowDx
+ * @see android.R.attr.shadowDy
+ * @see android.R.attr.shadowRadius
+ * @see android.R.attr.elegantTextHeight
+ * @see android.R.attr.letterSpacing
+ * @see android.R.attr.fontFeatureSettings
+ * @see android.R.attr.fontVariationSettings
+ */
 @JvmName("create")
 fun TextAppearanceSpanCompat(context: Context, @StyleRes appearance: Int): Any {
     return Factory(context, appearance)
